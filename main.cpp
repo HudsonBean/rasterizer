67d73226168bce164e38d57a3da4f547874b8896
Hudson Bean <hudsonbeandev@gmail.com>
2026-06-29 14:35:50 -0500

    Rendered a point to a screen.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  // Create window
   SDL_Window *window = SDL_CreateWindow("Rasterizer", 800, 600, 0);
   if (!window) {
     SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
@@ -13,6 +14,10 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  // Create renderer
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+
+  // Create event loop
   bool running = true;
   SDL_Event event;
   while (running) {
@@ -20,6 +25,13 @@ int main(int argc, char *argv[]) {
       if (event.type == SDL_EVENT_QUIT)
         running = false;
     }
+    // Draw point to screen
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // background
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderPoint(renderer, 100.0f, 100.0f);
+    SDL_RenderPresent(renderer);
   }
 
   SDL_DestroyWindow(window);

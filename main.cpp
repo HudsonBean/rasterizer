13265c9a4f995a54daf25df7cab48f1f7aabf740
Hudson Bean <hudsonbeandev@gmail.com>
2026-06-29 15:05:55 -0500

    Implemented custom framebuffer and can now draw pixels to the screen. Next implement bresenham's line drawing algorithm.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,18 @@
 #include <SDL3/SDL.h>
+#include <cstdint>
+#include <vector>
+
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+// Create framebuffer
+std::vector<uint32_t> framebuffer(WIDTH *HEIGHT);
+
+void place_pixel(int x, int y, uint32_t color) {
+  if (x >= 0 && y >= 0 && x < WIDTH && y < HEIGHT) {
+    framebuffer[y * WIDTH + x] = color; // Map color to grid spot
+  }
+}
 
 int main(int argc, char *argv[]) {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
@@ -7,7 +21,7 @@ int main(int argc, char *argv[]) {
   }
 
   // Create window
-  SDL_Window *window = SDL_CreateWindow("Rasterizer", 800, 600, 0);
+  SDL_Window *window = SDL_CreateWindow("Rasterizer", WIDTH, HEIGHT, 0);
   if (!window) {
     SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
     SDL_Quit();
@@ -16,6 +30,21 @@ int main(int argc, char *argv[]) {
 
   // Create renderer
   SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+  if (!renderer) {
+    SDL_Log("SDL_CreateRenderer failed: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  // Create texture
+  SDL_Texture *texture =
+      SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+                        SDL_TEXTUREACCESS_STREAMING, WIDTH, HEIGHT);
+  if (!texture) {
+    SDL_Log("SDL_CreateTexture failed: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
 
   // Create event loop
   bool running = true;
@@ -25,12 +54,22 @@ int main(int argc, char *argv[]) {
       if (event.type == SDL_EVENT_QUIT)
         running = false;
     }
-    // Draw point to screen
+    // Clear screen to black
+    for (auto &pixel : framebuffer) {
+      pixel = 0xFF000000;
+    }
 
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // background
-    SDL_RenderClear(renderer);
-    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-    SDL_RenderPoint(renderer, 100.0f, 100.0f);
+    // Draw pixel to center
+    place_pixel(WIDTH / 2, HEIGHT / 2, 0xFFFFFFFF);
+
+    // Update the texture
+    SDL_UpdateTexture(texture, nullptr, framebuffer.data(),
+                      WIDTH * sizeof(uint32_t));
+
+    // Draw texture to screen
+    SDL_RenderTexture(renderer, texture, nullptr, nullptr);
+
+    // Display
     SDL_RenderPresent(renderer);
   }
 

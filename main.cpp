07ef18a006c73ff9ef88a8a3b259efc1f2f76cfd
Hudson Bean <hudsonbeandev@gmail.com>
2026-06-29 00:05:51 -0500

    init

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <SDL3/SDL.h>
+
+int main(int argc, char *argv[]) {
+  if (!SDL_Init(SDL_INIT_VIDEO)) {
+    SDL_Log("SDL_Init failed: %s", SDL_GetError());
+    return 1;
+  }
+
+  SDL_Window *window = SDL_CreateWindow("Rasterizer", 800, 600, 0);
+  if (!window) {
+    SDL_Log("SDL_CreateWindow failed: %s", SDL_GetError());
+    SDL_Quit();
+    return 1;
+  }
+
+  bool running = true;
+  SDL_Event event;
+  while (running) {
+    while (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT)
+        running = false;
+    }
+  }
+
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+  return 0;
+}

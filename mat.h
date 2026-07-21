39f0794c3c18e4e982f07303dab0402da954b6aa
Hudson Bean <hudsonbeandev@gmail.com>
2026-07-21 13:58:03 -0500

    Implemented overload for matrix*matrix multiplication with Mat4 struct.

--- mat.h
+++ mat.h
@@ -13,6 +13,18 @@ struct Mat4 {
         v.x * mat[2][0] + v.y * mat[2][1] + v.z * mat[2][2] + v.w * mat[2][3],
         v.x * mat[3][0] + v.y * mat[3][1] + v.z * mat[3][2] + v.w * mat[3][3]);
   }
+
+  Mat4 operator*(const Mat4 &other) const {
+    Mat4 result{};
+    for (int i = 0; i < 4; i++) {
+      for (int j = 0; j < 4; j++) {
+        result.mat[i][j] =
+            mat[i][0] * other.mat[0][j] + mat[i][1] * other.mat[1][j] +
+            mat[i][2] * other.mat[2][j] + mat[i][3] * other.mat[3][j];
+      }
+    }
+    return result;
+  }
 };
 
 #endif
